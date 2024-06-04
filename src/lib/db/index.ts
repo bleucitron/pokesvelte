130907ed81e75502ecf21c711bfb4080281132af
Romain Crestey <rcrestey@gmail.com>
2024-06-04 21:48:55 +0200

    added course-specific starting files

---
+++ src/lib/db/index.ts
@@ -0,0 +1,11 @@
+import * as cookies from './cookies';
+import * as team from './team';
+import * as seen from './seen';
+import * as trainer from './trainer';
+
+export default {
+	team,
+	seen,
+	trainer,
+	cookies
+};
