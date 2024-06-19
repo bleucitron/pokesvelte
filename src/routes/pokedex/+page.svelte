131fb565411af1be6a2540699c6a56fbf1930702
Romain Crestey <rcrestey@gmail.com>
2024-06-19 23:55:26 +0200

    exercise: logic blocks

--- src/routes/pokedex/+page.svelte
+++ src/routes/pokedex/+page.svelte
@@ -4,12 +4,26 @@
 
 <h1>Pokédex</h1>
 
-<a href="/pokedex/1"
-	><img src={data.pokemons[0].sprites.front_default} alt={data.pokemons[0].name} /></a
->
-<a href="/pokedex/2"
-	><img src={data.pokemons[1].sprites.front_default} alt={data.pokemons[1].name} /></a
->
-<a href="/pokedex/3"
-	><img src={data.pokemons[2].sprites.front_default} alt={data.pokemons[2].name} /></a
->
+<ul>
+	{#each data.pokemons as pokemon}
+		{@const { id, name, sprites } = pokemon}
+		{@const src = sprites.front_default}
+
+		<li>
+			<a href="/pokedex/{id}"><img {src} alt={name} width="96" height="96" loading="lazy" /></a>
+		</li>
+	{/each}
+</ul>
+
+<style>
+	ul {
+		display: flex;
+		flex-wrap: wrap;
+		margin-block: 1rem;
+		gap: 1rem;
+	}
+
+	li:hover img {
+		filter: drop-shadow(0px 0px 10px #333);
+	}
+</style>
