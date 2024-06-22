a0e8f2321db914b304fee9a0e5fcb47f350bcc70
Romain Crestey <rcrestey@gmail.com>
2024-06-22 11:41:51 +0200

    exercise: event handlers

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,18 +1,49 @@
 <script lang="ts">
-	import Pokemon from '$lib/components/Pokemon.svelte';
+	import Wild from '$lib/components/Wild.svelte';
 
 	const started = false;
+	const choices = [1, 4, 7];
+
+	const { data } = $props();
 </script>
 
 <h1>Pokésvelte</h1>
 <p>Gotta svelt'em all!</p>
 
-{#if !started}
-	<Pokemon
-		id={25}
-		name="Pikachu"
-		src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png"
-	/>
-{:else}
-	<p>Work in progress</p>
-{/if}
+<div class="grass">
+	{#if !started}
+		<p>Choisissez un Pokémon</p>
+		<ul>
+			{#each choices as choice}
+				{@const pokemon = data.pokemons[choice - 1]}
+				{#if pokemon}
+					{@const { id, name, sprites } = pokemon}
+					<li>
+						<Wild
+							src={sprites.front_default}
+							catchPokemon={() => {
+								console.log(`Vous avez capturé un ${name} (id: ${id}) !`);
+							}}
+						/>
+					</li>
+				{/if}
+			{/each}
+		</ul>
+	{:else}
+		<p>Work in progress</p>
+	{/if}
+</div>
+
+<style>
+	.grass {
+		display: flex;
+		flex-flow: column;
+		justify-content: center;
+		flex: 1 0;
+	}
+
+	ul {
+		display: flex;
+		justify-content: center;
+	}
+</style>
