136c5de78bac77664e54d567513bc44ebd4d9cbf
Romain Crestey <rcrestey@gmail.com>
2024-06-22 09:51:13 +0200

    exercise: class directives

--- src/routes/pokedex/+page.svelte
+++ src/routes/pokedex/+page.svelte
@@ -5,11 +5,13 @@
 <h1>Pokédex</h1>
 
 <ul>
-	{#each data.pokemons as pokemon}
+	{#each data.pokemons as pokemon, i}
 		{@const { id, name, sprites } = pokemon}
 		{@const src = sprites.front_default}
 
-		<li>
+		{@const found = i % 2 === 0}
+
+		<li class:found>
 			<a href="/pokedex/{id}"><img {src} alt={name} width="96" height="96" loading="lazy" /></a>
 		</li>
 	{/each}
@@ -22,8 +24,17 @@
 		margin-block: 1rem;
 		gap: 1rem;
 	}
-
+	li img {
+		filter: contrast(0%) brightness(200%);
+	}
 	li:hover img {
+		filter: contrast(0%) brightness(200%) drop-shadow(0px 0px 10px #333);
+	}
+
+	li.found img {
+		filter: none;
+	}
+	li.found:hover img {
 		filter: drop-shadow(0px 0px 10px #333);
 	}
 </style>
