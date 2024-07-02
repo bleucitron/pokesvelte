42bd79f66552be857ca0772a921a45d5d073b3b4
Romain Crestey <rcrestey@gmail.com>
2024-07-02 21:42:04 +0200

    exercise: each block bindings

--- src/routes/team/+page.svelte
+++ src/routes/team/+page.svelte
@@ -5,6 +5,9 @@
 
 	const { pokemons, team } = $derived(data);
 
+	const main = $derived(team.filter((p) => p.main));
+	const other = $derived(team.filter((p) => !p.main));
+
 	async function release(uuid: string) {
 		await fetch(`/team/${uuid}`, { method: 'DELETE' });
 		invalidate('team:update');
@@ -13,15 +16,32 @@
 
 <h1>Mon équipe</h1>
 
+<h2>Titulaires</h2>
 <ul>
-	{#each team as member}
+	{#each main as member}
 		{@const { id, uuid } = member}
 		{@const { name, sprites } = pokemons[id - 1]}
 		{@const src = sprites.front_default}
 
 		<li>
 			<img {src} alt={name} width="96" height="96" loading="lazy" />
-			<p>{uuid}</p>
+			<input bind:value={member.name} />
+			<label>Sélection<input type="checkbox" bind:checked={member.main} /></label>
+			<button onclick={() => release(uuid)}>x</button>
+		</li>
+	{/each}
+</ul>
+<h2>Remplaçants</h2>
+<ul>
+	{#each other as member}
+		{@const { id, uuid } = member}
+		{@const { name, sprites } = pokemons[id - 1]}
+		{@const src = sprites.front_default}
+
+		<li>
+			<img {src} alt={name} width="96" height="96" loading="lazy" />
+			<input bind:value={member.name} />
+			<label>Sélection<input type="checkbox" bind:checked={member.main} /></label>
 			<button onclick={() => release(uuid)}>x</button>
 		</li>
 	{/each}
@@ -32,11 +52,30 @@
 		display: flex;
 		flex-wrap: wrap;
 		margin-block: 1rem;
-		gap: 1rem;
+		gap: 2rem;
 	}
 
 	li {
+		position: relative;
 		display: flex;
+		flex-flow: column;
 		align-items: center;
+		gap: 0.5rem;
+	}
+	li button {
+		position: absolute;
+		top: 0;
+		right: 0;
+	}
+
+	input:not([type='checkbox']) {
+		width: 12rem;
+		text-align: center;
+	}
+
+	label {
+		display: flex;
+		gap: 0.5rem;
+		align-items: baseline;
 	}
 </style>
