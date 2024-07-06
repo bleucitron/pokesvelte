208a0949fb70ecb9d29a34b054ce2b5ad9286c0d
Romain Crestey <rcrestey@gmail.com>
2024-07-06 10:02:22 +0200

    exercise: form actions

--- src/routes/trainer/+page.server.ts
+++ src/routes/trainer/+page.server.ts
@@ -1,3 +1,16 @@
+import { saveTrainer } from '$lib/server/trainer';
+
 export async function load() {
-	throw new Error('Pas de dresseur !');
+	console.log('Loading trainer');
 }
+
+export const actions = {
+	default: async ({ request }) => {
+		const data = await request.formData();
+
+		const name = data.get('name');
+		const password = data.get('password');
+
+		await saveTrainer(name, password);
+	}
+};
