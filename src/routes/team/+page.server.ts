0520d31366fdb8201a1ba42e4daa5c43bfc80479
Romain Crestey <rcrestey@gmail.com>
2024-07-06 13:57:31 +0200

    exercise: named actions

--- src/routes/team/+page.server.ts
+++ src/routes/team/+page.server.ts
@@ -1,5 +1,30 @@
 import { fetchPokemons } from '$lib/pokemons';
 import db from '$lib/server/db';
+import { fail } from '@sveltejs/kit';
+
+export const actions = {
+	rename: async ({ request }) => {
+		const data = await request.formData();
+		const uuid = data.get('uuid')?.toString();
+		const name = data.get('name')?.toString();
+
+		if (!uuid || !name) {
+			return fail(400, { uuid, name, missing: true });
+		}
+
+		await db.team.renameMember(uuid, name);
+	},
+	toggle: async ({ request }) => {
+		const data = await request.formData();
+		const uuid = data.get('uuid')?.toString();
+
+		if (!uuid) {
+			return fail(400, { uuid, missing: true });
+		}
+
+		await db.team.toggleMember(uuid);
+	}
+};
 
 export async function load({ depends }) {
 	depends('team:update');
--- src/routes/team/+page.svelte
+++ src/routes/team/+page.svelte
@@ -20,18 +20,26 @@
 <h2>Titulaires</h2>
 <ul>
 	{#each main as member}
-		{@const { id, uuid } = member}
+		{@const { id, uuid, main, name } = member}
 		{@const pokemon = pokemons[id - 1]}
 		{@const recentMember = recent.members.includes(uuid)}
 		{#if pokemon}
-			{@const { name, sprites } = pokemon}
+			{@const { sprites, name: speciesName } = pokemon}
 			{@const src = sprites.front_default}
 
 			<li>
-				<img {src} alt={name} width="96" height="96" loading="lazy" />
-				<input bind:value={member.name} />
-				<label>Sélection<input type="checkbox" bind:checked={member.main} /></label>
+				<img {src} alt={speciesName} width="96" height="96" loading="lazy" />
+				<form action="?/rename" method="POST">
+					<input name="name" value={name} />
+					<input name="uuid" type="hidden" value={uuid} />
+					<button>Renommer</button>
+					<button class:main formaction="?/toggle">Titulariser</button>
+				</form>
 				<button onclick={() => release(uuid)}>x</button>
+
+				{#if recentMember}
+					<div class="new">new</div>
+				{/if}
 			</li>
 		{/if}
 	{/each}
@@ -39,16 +47,21 @@
 <h2>Remplaçants</h2>
 <ul>
 	{#each other as member}
-		{@const { id, uuid } = member}
+		{@const { id, uuid, main, name } = member}
 		{@const pokemon = pokemons[id - 1]}
+		{@const recentMember = recent.members.includes(uuid)}
 		{#if pokemon}
-			{@const { name, sprites } = pokemon}
+			{@const { sprites, name: speciesName } = pokemon}
 			{@const src = sprites.front_default}
 
 			<li>
-				<img {src} alt={name} width="96" height="96" loading="lazy" />
-				<input bind:value={member.name} />
-				<label>Sélection<input type="checkbox" bind:checked={member.main} /></label>
+				<img {src} alt={speciesName} width="96" height="96" loading="lazy" />
+				<form action="?/rename" method="POST">
+					<input name="name" value={name} />
+					<input name="uuid" type="hidden" value={uuid} />
+					<button>Renommer</button>
+					<button class:main formaction="?/toggle">Titulariser</button>
+				</form>
 				<button onclick={() => release(uuid)}>x</button>
 
 				{#if recentMember}
@@ -74,10 +87,20 @@
 		align-items: center;
 		gap: 0.5rem;
 	}
-	li button {
+	li > button {
 		position: absolute;
 		top: 0;
 		right: 0;
+	}
+
+	form {
+		display: flex;
+		flex-flow: column;
+	}
+
+	form > button.main {
+		background: #888;
+		color: white;
 	}
 
 	input:not([type='checkbox']) {
@@ -85,11 +108,6 @@
 		text-align: center;
 	}
 
-	label {
-		display: flex;
-		gap: 0.5rem;
-		align-items: baseline;
-	}
 	li .new {
 		position: absolute;
 		content: 'new';
