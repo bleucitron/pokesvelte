cfd12db545d74863afabb75b50c00f992f7f4c91
Romain Crestey <rcrestey@gmail.com>
2024-07-07 18:27:05 +0200

    exercise: errors

--- src/routes/pokedex/[id]/+page.server.ts
+++ src/routes/pokedex/[id]/+page.server.ts
@@ -1,9 +1,15 @@
+import { error } from '@sveltejs/kit';
+
 import db from '$lib/server/db';
+
 import { fetchPokemon } from '$lib/pokemons';
 
 export async function load({ params }) {
 	const [pokemon, seen] = await Promise.all([fetchPokemon(params.id), db.seen.get()]);
 
+	if (!pokemon) {
+		error(404, { message: `Le Pokémon n°${params.id}  n'existe pas` });
+	}
 	const { id, name, sprites } = pokemon;
 	const found = seen.includes(id);
 
