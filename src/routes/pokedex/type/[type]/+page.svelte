b352b7f2f8b80f2dcc1fbcacaad8b5202052475d
Romain Crestey <rcrestey@gmail.com>
2024-07-11 23:01:35 +0200

    exercise: nested layouts

--- src/routes/pokedex/type/[type]/+page.svelte
+++ src/routes/pokedex/type/[type]/+page.svelte
@@ -1,9 +1,7 @@
 <script lang="ts">
-	import { recent } from '$lib/stores/index.svelte';
+	let { data } = $props();
 
-	const { data } = $props();
-
-	const { pokemons, seen } = $derived(data);
+	let { pokemons, seen } = $derived(data);
 	let search = $state('');
 
 	const filtered = $derived(pokemons.filter((pokemon) => pokemon.name.includes(search)));
@@ -11,15 +9,15 @@
 </script>
 
 <input bind:value={search} placeholder="recherche..." />
+
 <ul>
 	{#each toDisplay as pokemon}
 		{@const { id, name, sprites } = pokemon}
 		{@const src = sprites.front_default}
 
 		{@const found = seen.includes(id)}
-		{@const recentSpecies = recent.species.includes(id)}
 
-		<li class:found class:recent={recentSpecies}>
+		<li class:found>
 			<a href="/pokedex/{id}"><img {src} alt={name} width="96" height="96" loading="lazy" /></a>
 		</li>
 	{/each}
@@ -31,9 +29,6 @@
 		flex-wrap: wrap;
 		margin-block: 1rem;
 		gap: 1rem;
-	}
-	li {
-		position: relative;
 	}
 	li img {
 		filter: contrast(0%) brightness(200%);
@@ -48,20 +43,6 @@
 	li.found:hover img {
 		filter: drop-shadow(0px 0px 10px #333);
 	}
-	li.recent::before {
-		position: absolute;
-		content: 'new';
-		top: 0;
-		left: 0;
-		background: orange;
-		border-radius: 8px;
-		padding: 0.2rem;
-		padding-top: 0.4rem;
-		font-size: 0.7rem;
-		border: 2px solid #888;
-		line-height: 0.8rem;
-	}
-
 	input {
 		margin-block: 1rem;
 	}
