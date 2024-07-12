ed9ea4b33dbb5546d7c56ea5a073f0c64bfc7f0e
Romain Crestey <rcrestey@gmail.com>
2024-07-12 23:28:56 +0200

    exercise: hooks

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,9 +1,11 @@
-// See https://kit.svelte.dev/docs/types#app
-// for information about these interfaces
+import type { Trainer } from '$lib/server/trainer';
+
 declare global {
 	namespace App {
 		// interface Error {}
-		// interface Locals {}
+		interface Locals {
+			user: Trainer | null;
+		}
 		// interface PageData {}
 		// interface PageState {}
 		// interface Platform {}
--- src/routes/team/+server.ts
+++ src/routes/team/+server.ts
@@ -1,7 +1,10 @@
-import { json } from '@sveltejs/kit';
+import { error, json } from '@sveltejs/kit';
 import db from '$lib/server/db';
 
-export async function POST({ request }) {
+export async function POST({ request, locals }) {
+	if (!locals.user) {
+		error(403, "Vous n'avez pas le droit de faire ça");
+	}
 	const { id } = await request.json();
 
 	const newMember = await db.team.addMember(id);
