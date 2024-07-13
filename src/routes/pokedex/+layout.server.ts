c5204854b70800688385d1d69a5b44d6213a6a88
Romain Crestey <rcrestey@gmail.com>
2024-07-13 23:24:17 +0200

    exercise: page options

--- src/routes/pokedex/+layout.server.ts
+++ src/routes/pokedex/+layout.server.ts
@@ -3,6 +3,9 @@ import { fetchPokemonTypes } from '$lib/pokemons';
 import db from '$lib/server/db';
 import { fetchPokemons } from '$lib/pokemons';
 
+// décommenter la ligne suivante pour constater que le filtre par nom ne fonctionne plus
+// export const csr = false;
+
 export async function load({ params }) {
 	const [pokemons, types, seen] = await Promise.all([
 		fetchPokemons(),
