f877670cbaf4cc08e11349479fd4a4adb20f1cb0
Romain Crestey <rcrestey@gmail.com>
2024-07-15 16:55:02 +0200

    exercise: syntax basics

---
+++ src/lib/Pokemon.svelte
@@ -0,0 +1,5 @@
+<p>#25</p>
+
+<img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png" />
+
+<h1>Pikachu</h1>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,8 @@
+<script>
+	import Pokemon from '$lib/Pokemon.svelte';
+</script>
+
 <h1>Pokésvelte</h1>
 <p>Gotta svelt'em all!</p>
+
+<Pokemon />
