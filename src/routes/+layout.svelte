4f0532c99f2f21f3911ba257da1e8dd2432bb322
Romain Crestey <rcrestey@gmail.com>
2024-07-15 11:46:51 +0200

    exercise: basic layout

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	const { children } = $props();
+</script>
+
+<header>
+	<nav>
+		<ul>
+			<li>
+				<a href="/">Accueil</a>
+				<a href="/pokedex">Pokédex</a>
+				<a href="/team">Team</a>
+				<a href="/faq">FAQ</a>
+			</li>
+		</ul>
+	</nav>
+</header>
+
+<main>
+	{@render children()}
+</main>
+
+<footer>PokéSvelte</footer>
