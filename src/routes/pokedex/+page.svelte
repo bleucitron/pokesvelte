4c2d1d665d0331ee24019828830dbc9997068a52
Romain Crestey <rcrestey@gmail.com>
2024-07-15 14:05:13 +0200

    exercise: page data loading

--- src/routes/pokedex/+page.svelte
+++ src/routes/pokedex/+page.svelte
@@ -1,1 +1,9 @@
+<script lang="ts">
+	const { data } = $props();
+</script>
+
 <h1>Pokédex</h1>
+
+<img src={data.pokemons[0]?.sprites.front_default} />
+<img src={data.pokemons[1]?.sprites.front_default} />
+<img src={data.pokemons[2]?.sprites.front_default} />
