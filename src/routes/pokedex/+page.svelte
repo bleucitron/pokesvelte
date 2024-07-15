7d6ac43a8a129ce4a4c9677dbf6cd52550d70ed4
Romain Crestey <rcrestey@gmail.com>
2024-07-15 15:48:05 +0200

    exercise: dynamic routing

--- src/routes/pokedex/+page.svelte
+++ src/routes/pokedex/+page.svelte
@@ -4,6 +4,12 @@
 
 <h1>Pokédex</h1>
 
-<img src={data.pokemons[0]?.sprites?.front_default} alt={data.pokemons[0]?.name} />
-<img src={data.pokemons[1]?.sprites?.front_default} alt={data.pokemons[1]?.name} />
-<img src={data.pokemons[2]?.sprites?.front_default} alt={data.pokemons[2]?.name} />
+<a href="/pokedex/1"
+	><img src={data.pokemons[0]?.sprites.front_default} alt={data.pokemons[0]?.name} /></a
+>
+<a href="/pokedex/2"
+	><img src={data.pokemons[1]?.sprites.front_default} alt={data.pokemons[1]?.name} /></a
+>
+<a href="/pokedex/3"
+	><img src={data.pokemons[2]?.sprites.front_default} alt={data.pokemons[2]?.name} /></a
+>
