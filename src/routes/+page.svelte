4757506cc43fb0352041f9b01a6e346a8a93c0a6
Romain Crestey <rcrestey@gmail.com>
2024-07-16 17:20:14 +0200

    exercise: effects

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,27 @@
 <script lang="ts">
 	import Wild from '$lib/Wild.svelte';
 	import { pokedex } from '$lib/stores/index.svelte';
+	import { getRandomNb } from '$lib/utils';
 
 	const { data } = $props();
 	const { pokemons } = $derived(data);
 
 	const found = $derived(pokedex.found);
 	const started = $derived(found.length > 0);
+
+	let wild = $state(25);
+
+	$effect(() => {
+		if (started) {
+			setInterval(() => {
+				const randomId = getRandomNb(151) + 1;
+				console.log('interval', randomId, pokemons?.[randomId - 1]?.name);
+
+				wild = randomId;
+			}, 2000);
+		}
+		console.log(started);
+	});
 
 	function catchPokemon(id: number, name?: string) {
 		pokedex.discover(id);
@@ -23,16 +38,20 @@
 
 {#if !started}
 	<div class="house">
-		{#each [1, 4, 7] as wildId}
-			{@const pokemon = pokemons[wildId - 1]}
+		{#each [1, 4, 7] as starterId}
+			{@const pokemon = pokemons[starterId - 1]}
 			{@const src = pokemon?.sprites?.front_default}
 			{@const name = pokemon?.name}
 
-			<Wild {src} {name} catchPokemon={() => catchPokemon(wildId, name)} />
+			<Wild {src} {name} catchPokemon={() => catchPokemon(starterId, name)} />
 		{/each}
 	</div>
 {:else}
-	<p>Le jeu est commencé</p>
+	{@const pokemon = pokemons[wild - 1]}
+	{@const src = pokemon?.sprites?.front_default}
+	{@const name = pokemon?.name}
+
+	<Wild {src} {name} catchPokemon={() => catchPokemon(wild, name)} />
 {/if}
 
 <style>
