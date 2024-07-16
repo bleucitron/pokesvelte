33fc5cfff52c17b88200a155af34eafec4d3893a
Romain Crestey <rcrestey@gmail.com>
2024-07-16 13:48:41 +0200

    exercise: event handlers

---
+++ src/lib/Wild.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	type WildProps = {
+		src: string | undefined;
+		catchPokemon: () => void;
+	};
+	const { src, catchPokemon }: WildProps = $props();
+</script>
+
+<button class="Wild" onclick={catchPokemon}>
+	<img {src} />
+</button>
+
+<style>
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,18 +1,33 @@
-<script>
-	import Pokemon from '$lib/Pokemon.svelte';
+<script lang="ts">
+	import Wild from '$lib/Wild.svelte';
 
+	const { data } = $props();
 	let started = false;
+
+	function logName(name?: string) {
+		console.log('attrapé', name);
+	}
 </script>
 
 <h1>Pokésvelte</h1>
 <p>Gotta svelt'em all!</p>
 
 {#if !started}
-	<Pokemon
-		name="Pikachu"
-		id={25}
-		src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png"
-	/>
+	<div class="house">
+		{#each [1, 4, 7] as wildId}
+			{@const pokemon = data.pokemons[wildId - 1]}
+			{@const src = pokemon?.sprites?.front_default}
+			{@const name = pokemon?.name}
+
+			<Wild catchPokemon={() => logName(name)} {src} />
+		{/each}
+	</div>
 {:else}
 	<p>Le jeu est commencé</p>
 {/if}
+
+<style>
+	.house {
+		display: flex;
+	}
+</style>
