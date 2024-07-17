7ba0262ac06bd29516fa588bc1c9adb391575602
Romain Crestey <rcrestey@gmail.com>
2024-07-17 17:06:27 +0200

    exercise: errors and redirects

--- src/routes/pokedex/[id]/+page.server.ts
+++ src/routes/pokedex/[id]/+page.server.ts
@@ -2,11 +2,15 @@ import type { PageServerLoad } from './$types';
 
 import { fetchPokemon } from '$lib/server/pokemons';
 import { readSeen } from '$lib/server/seen';
+import { error } from '@sveltejs/kit';
 
 export const load: PageServerLoad = async ({ params }) => {
 	const id = parseInt(params.id);
 
 	const pokemon = await fetchPokemon(id);
+	if (!pokemon) {
+		error(404, { message: 'Pokémon introuvable' });
+	}
 	const seen = await readSeen();
 
 	return {
--- src/routes/team/+page.server.ts
+++ src/routes/team/+page.server.ts
@@ -1,9 +1,13 @@
 import { fetchPokemons } from '$lib/server/pokemons';
 import { readTeam } from '$lib/server/team';
+import { redirect } from '@sveltejs/kit';
 
 export async function load() {
 	const pokemons = await fetchPokemons();
 	const team = await readTeam();
 
+	if (team.length === 0) {
+		redirect(307, '/');
+	}
 	return { pokemons, team };
 }
