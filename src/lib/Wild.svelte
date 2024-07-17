34631d1504076db2ad3075d195792b4e7e98a71e
Romain Crestey <rcrestey@gmail.com>
2024-07-17 15:23:50 +0200

    exercise: other bindings

--- src/lib/Wild.svelte
+++ src/lib/Wild.svelte
@@ -7,6 +7,10 @@
 	};
 	const { name, src, escape, catchPokemon }: WildProps = $props();
 
+	let width = $state(0);
+	let height = $state(0);
+	$inspect(width, height);
+
 	$effect(() => {
 		const timeoutId = escape
 			? setTimeout(() => {
@@ -17,6 +21,8 @@
 		return () => clearTimeout(timeoutId);
 	});
 </script>
+
+<svelte:window bind:innerWidth={width} bind:innerHeight={height} />
 
 <button class="Wild" onclick={catchPokemon}>
 	<img {src} alt={name} />
