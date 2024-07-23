454783371b68ac5749e7478e2833f2a78f5143f7
Romain Crestey <rcrestey@gmail.com>
2024-07-23 18:30:03 +0200

    refactor: move utils to $lib/server

---
+++ src/lib/server/index.ts
@@ -0,0 +1,53 @@
+import type { MarkdownItEnv } from '@mdit-vue/types';
+
+import { readFile, readdir } from 'fs/promises';
+import { join } from 'path';
+import markdownit from 'markdown-it';
+import { frontmatterPlugin } from '@mdit-vue/plugin-frontmatter';
+import Shiki from '@shikijs/markdown-it';
+
+const md = markdownit({ html: true })
+	.use(
+		await Shiki({
+			themes: {
+				light: 'vitesse-light',
+				dark: 'vitesse-dark'
+			}
+		})
+	)
+	.use(frontmatterPlugin);
+
+export async function parseMdFile(path: string) {
+	const content = (await readFile(join('course', `${path}.md`))).toString();
+	const env: MarkdownItEnv = {};
+
+	const rendered = md.render(content, env);
+
+	return { content: rendered, options: env.frontmatter };
+}
+
+export type Node = {
+	name: string;
+	path: string;
+	files?: Node[];
+};
+
+export async function readDir(path: string): Promise<Node[]> {
+	const dir = await readdir(path, { withFileTypes: true });
+
+	const entries = await Promise.all(
+		dir.map(async (item) => {
+			const { name, parentPath } = item;
+			const path = join(parentPath, name);
+			const files = item.isDirectory() ? await readDir(path) : undefined;
+
+			return {
+				name,
+				path: path.replace('.md', '').replace('course', ''),
+				files
+			};
+		})
+	);
+
+	return entries;
+}
