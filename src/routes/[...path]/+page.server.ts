c9ddfaf50d8766b594f4ea1bb5f8c99f9f6df788
Romain Crestey <rcrestey@gmail.com>
2024-07-24 20:51:47 +0200

    feat: handle parent link

--- src/routes/[...path]/+page.server.ts
+++ src/routes/[...path]/+page.server.ts
@@ -1,10 +1,18 @@
-import { parseMdFile } from '$lib/server';
+import { CONTENT_FOLDER } from '$lib/constants';
+import { normalizePath } from '$lib/helpers';
+import { parseMdFile, findCurrent, readDir } from '$lib/server';
+import { dirname, join } from 'path';
 
-export async function load({ params: { path } }) {
-	const { content, options } = await parseMdFile(path);
+export async function load({ params: { path }, parent }) {
+	const [mdContent, { tree }] = await Promise.all([parseMdFile(path), parent()]);
+
+	const found = mdContent && findCurrent(path, tree);
+	const localTree = await readDir(join(CONTENT_FOLDER, mdContent ? dirname(path) : path));
 
 	return {
-		content,
-		options
+		...mdContent,
+		...found,
+		localTree,
+		parentLink: normalizePath(dirname(path))
 	};
 }
