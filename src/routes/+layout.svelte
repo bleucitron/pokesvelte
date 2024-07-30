7add4cdc6c7e7a5df2fcf4c66913962867d3bb0e
Romain Crestey <rcrestey@gmail.com>
2024-07-30 20:25:36 +0200

    feat: better navigation

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	const { children } = $props();
+</script>
+
+<main>
+	{@render children()}
+</main>
+
+<style>
+	main {
+		width: 100%;
+		min-height: 100%;
+		display: flex;
+		flex-flow: column;
+		padding: 1rem 1.5rem;
+		margin: auto;
+	}
+
+	@media (min-width: 1024px) {
+		main {
+			width: 80ch;
+		}
+	}
+</style>
