f4072e82a41d2324c89b5722ee1fe72c0a22e1b6
Romain Crestey <rcrestey@gmail.com>
2024-08-19 22:16:20 +0200

    simplify data loaded

--- src/routes/[...path]/+page.server.ts
+++ src/routes/[...path]/+page.server.ts
@@ -1,15 +1,11 @@
-import { parseMdFile, findCurrent } from '$lib/server';
+import { findCurrent } from '$lib/server';
 import { redirect } from '@sveltejs/kit';
 
 export async function load({ params: { path }, parent }) {
-	const [mdContent, { tree }] = await Promise.all([parseMdFile(path), parent()]);
-
+	const { tree } = await parent();
 	const found = findCurrent(path, tree);
 
 	if (!found) redirect(307, '/');
 
-	return {
-		...mdContent,
-		...found
-	};
+	return found;
 }
