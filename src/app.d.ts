a3add78d21685c151aa8fa368d21d7acdc89fa89
Romain Crestey <rcrestey@gmail.com>
2024-09-03 20:30:13 +0200

    fix: set PageData type

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,10 +1,16 @@
-// See https://kit.svelte.dev/docs/types#app
+import type { Node } from '$lib/typings';
+
 // for information about these interfaces
 declare global {
 	namespace App {
 		// interface Error {}
 		// interface Locals {}
-		// interface PageData {}
+		interface PageData {
+			current?: Node;
+			prev?: Node;
+			next?: Node;
+			parent?: Node;
+		}
 		// interface PageState {}
 		// interface Platform {}
 	}
