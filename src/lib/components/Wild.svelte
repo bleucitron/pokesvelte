aef54ba2e91aa21f2d59b856660cc6ef895f1ec7
Romain Crestey <rcrestey@gmail.com>
2025-12-24 15:36:43 +0100

    exercise: event handlers

---
+++ src/lib/components/Wild.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	type WildProps = {
+		src: string;
+		catchPokemon: () => void;
+	};
+	const { src, catchPokemon }: WildProps = $props();
+</script>
+
+<button class="Wild" onclick={catchPokemon}>
+	<img {src} />
+</button>
+
+<style>
+	.Wild {
+		width: 15rem;
+		background: inherit;
+	}
+
+	img {
+		height: 15rem;
+		object-fit: cover;
+	}
+	img:hover {
+		filter: drop-shadow(0px 0px 10px #333);
+	}
+</style>
