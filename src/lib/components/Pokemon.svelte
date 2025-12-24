d281e1110b658ff97c9f2a35b6dff4721ee1ee71
Romain Crestey <rcrestey@gmail.com>
2025-12-24 14:13:44 +0100

    exercise: syntax basics

---
+++ src/lib/components/Pokemon.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+	const id = 133;
+	const name = 'Évoli';
+	const image = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/133.png';
+</script>
+
+<div class="Pokemon">
+	<p>#{id}</p>
+	<img src={image} />
+	<p>{name}</p>
+</div>
+
+<style>
+	.Pokemon {
+		display: flex;
+		flex-flow: column;
+		align-items: center;
+		width: 15rem;
+		margin: auto;
+	}
+
+	p {
+		margin: 0;
+		font-size: 1.5rem;
+		text-transform: capitalize;
+	}
+
+	img {
+		height: 15rem;
+		object-fit: cover;
+	}
+</style>
