f4a7f4153f1f019605926f24e0109ca5f7128e11
Romain Crestey <rcrestey@gmail.com>
2025-12-24 15:25:27 +0100

    exercise: class powers

--- src/lib/components/Pokemon.svelte
+++ src/lib/components/Pokemon.svelte
@@ -3,11 +3,12 @@
 		id: number;
 		name: string;
 		src: string;
+		found?: boolean;
 	};
-	const { id, name, src }: Pokemon = $props();
+	const { id, name, src, found = false }: Pokemon = $props();
 </script>
 
-<div class="Pokemon">
+<div class={['Pokemon', { found }]}>
 	<p>#{id}</p>
 	<img {src} />
 	<p>{name}</p>
@@ -21,15 +22,17 @@
 		width: 15rem;
 		margin: auto;
 	}
-
 	p {
 		margin: 0;
 		font-size: 1.5rem;
 		text-transform: capitalize;
 	}
-
 	img {
 		height: 15rem;
 		object-fit: cover;
+		filter: contrast(0%) brightness(200%);
+	}
+	.found img {
+		filter: none;
 	}
 </style>
--- src/routes/pokedex/+page.svelte
+++ src/routes/pokedex/+page.svelte
@@ -10,7 +10,7 @@
 	{#each data.pokemons as pokemon}
 		{@const { id, sprites } = pokemon}
 		{@const src = sprites.front_default}
-		<li>
+		<li class={{ found: id % 2 === 0 }}>
 			<a href={resolve('/pokedex/[id]', { id: id.toString() })}>
 				<img {src} width="96" height="96" loading="lazy" />
 			</a>
@@ -26,7 +26,16 @@
 		gap: 1rem;
 	}
 
+	li img {
+		filter: contrast(0%) brightness(200%);
+	}
 	li:hover img {
+		filter: contrast(0%) brightness(200%) drop-shadow(0px 0px 10px #333);
+	}
+	li.found img {
+		filter: none;
+	}
+	li.found:hover img {
 		filter: drop-shadow(0px 0px 10px #333);
 	}
 </style>
