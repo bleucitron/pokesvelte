361ce1fc485e0a5156e29a601a13fbd570b34b75
Romain Crestey <rcrestey@gmail.com>
2026-01-16 14:24:26 +0100

    exercise: form validation

--- src/routes/trainer/+page.server.ts
+++ src/routes/trainer/+page.server.ts
@@ -1,4 +1,5 @@
 import db from '$lib/server/db';
+import { fail } from '@sveltejs/kit';
 
 export async function load() {
 	console.log('TRAINER');
@@ -10,9 +11,47 @@ export const actions = {
 
 		const name = data.get('name')?.toString();
 		const password = data.get('password')?.toString();
+		const passwordConfirmation = data.get('password-confirmation')?.toString();
 
-		if (name && password) {
-			db.trainer.register(name, password);
+		if (!name) {
+			return fail(400, {
+				values: { name },
+				errorFields: ['signup_name'],
+				message: 'Le nom est manquant'
+			});
 		}
+		if (!password) {
+			return fail(400, {
+				values: { name },
+				errorFields: ['password'],
+				message: 'Le mot de passe est manquant'
+			});
+		}
+		if (password.length < 8) {
+			return fail(400, {
+				values: { name },
+				errorFields: ['password'],
+				message: 'Le mot de passe est trop court'
+			});
+		}
+		if (password !== passwordConfirmation) {
+			return fail(400, {
+				values: { name },
+				errorFields: ['password', 'password-confirmation'],
+				message: 'Les mots de passe ne correspondent pas'
+			});
+		}
+		const alreadyExists = await db.trainer.get(name);
+		if (alreadyExists) {
+			return fail(400, {
+				values: { name },
+				errorFields: ['name'],
+				message: 'Le nom est déjà utilisé'
+			});
+		}
+
+		const trainer = await db.trainer.register(name, password);
+
+		return { success: true, trainer };
 	}
 };
--- src/routes/trainer/+page.svelte
+++ src/routes/trainer/+page.svelte
@@ -1,18 +1,35 @@
+<script lang="ts">
+	const { form } = $props();
+
+	const errorFields = $derived(form?.errorFields ?? []);
+	const trainer = $derived(form?.trainer);
+</script>
+
 <h1>Dresseur</h1>
 
 <h2>Inscription</h2>
 
 <form method="POST">
-	<label>
+	<label class={{ error: errorFields.includes('name') }}>
 		Nom
-		<input name="name" />
+		<input name="name" value={form?.values?.name} />
 	</label>
-	<label>
+	<label class={{ error: errorFields.includes('password') }}>
 		Mot de passe
 		<input type="password" name="password" />
 	</label>
+	<label class={{ error: errorFields.includes('password-confirmation') }}>
+		Confirmation du mot de passe
+		<input type="password" name="password-confirmation" />
+	</label>
 	<button>S'incrire</button>
 </form>
+
+{#if form?.message}
+	<p class="error">{form.message}</p>
+{:else if form?.success}
+	<p>Utilisateur {trainer?.name} ({trainer?.id}) créé avec succès !</p>
+{/if}
 
 <style>
 	form {
@@ -32,5 +49,13 @@
 	}
 	button {
 		margin-block: 0.5rem;
+	}
+
+	.error {
+		color: red;
+
+		input {
+			outline: 2px solid red;
+		}
 	}
 </style>
