b3d8fbb428a7ff73457eca513b5a0d7153e43fa9
Romain Crestey <rcrestey@gmail.com>
2026-01-19 15:39:40 +0100

    exercice: named actions

--- src/routes/team/+page.server.ts
+++ src/routes/team/+page.server.ts
@@ -1,5 +1,6 @@
 import { fetchPokemons } from '$lib/pokemons';
 import db from '$lib/server/db';
+import { fail } from '@sveltejs/kit';
 
 export async function load({ depends }) {
 	depends('team:update');
@@ -8,3 +9,29 @@ export async function load({ depends }) {
 
 	return { team, pokemons };
 }
+
+export const actions = {
+	rename: async ({ request }) => {
+		const data = await request.formData();
+
+		const name = data.get('name')?.toString();
+		const uuid = data.get('uuid')?.toString();
+
+		if (!uuid || !name) {
+			return fail(400);
+		}
+
+		await db.team.renameMember(uuid, name);
+	},
+	toggle: async ({ request }) => {
+		const data = await request.formData();
+
+		const uuid = data.get('uuid')?.toString();
+
+		if (!uuid) {
+			return fail(400);
+		}
+
+		await db.team.toggleMember(uuid);
+	}
+};
--- src/routes/trainer/+page.svelte
+++ src/routes/trainer/+page.svelte
@@ -2,36 +2,60 @@
 	const { form } = $props();
 
 	const errorFields = $derived(form?.errorFields ?? []);
-	const trainer = $derived(form?.trainer);
 </script>
 
 <h1>Dresseur</h1>
 
-<h2>Inscription</h2>
+<div class="forms">
+	<div>
+		<h2>Inscription</h2>
 
-<form method="POST">
-	<label class={{ error: errorFields.includes('name') }}>
-		Nom
-		<input name="name" value={form?.values?.name} />
-	</label>
-	<label class={{ error: errorFields.includes('password') }}>
-		Mot de passe
-		<input type="password" name="password" />
-	</label>
-	<label class={{ error: errorFields.includes('password-confirmation') }}>
-		Confirmation du mot de passe
-		<input type="password" name="password-confirmation" />
-	</label>
-	<button>S'incrire</button>
-</form>
+		<form method="POST" action="?/signup">
+			<label class={{ error: errorFields.includes('signup_name') }}>
+				Nom
+				<input name="signup_name" value={form?.signup_name} />
+			</label>
+			<label class={{ error: errorFields.includes('signup_password') }}>
+				Mot de passe
+				<input type="password" name="signup_password" />
+			</label>
+			<label class={{ error: errorFields.includes('signup_password-confirmation') }}>
+				Confirmation du mot de passe
+				<input type="password" name="signup_password-confirmation" />
+			</label>
+			<button>S'incrire</button>
+		</form>
+	</div>
+	<div>
+		<h2>Connexion</h2>
 
+		<form method="POST" action="?/login">
+			<label class={{ error: errorFields.includes('login_name') }}>
+				Nom
+				<input name="login_name" value={form?.login_name} />
+			</label>
+			<label class={{ error: errorFields.includes('login_password') }}>
+				Mot de passe
+				<input type="password" name="login_password" />
+			</label>
+			<button>Se connecter</button>
+		</form>
+	</div>
+</div>
 {#if form?.message}
-	<p class="error">{form.message}</p>
-{:else if form?.success}
-	<p>Utilisateur {trainer?.name} ({trainer?.id}) créé avec succès !</p>
+	<p class={{ error: !form?.success }}>{form.message}</p>
 {/if}
 
 <style>
+	.forms {
+		display: flex;
+		gap: 5rem;
+	}
+
+	h2 {
+		text-align: center;
+	}
+
 	form {
 		display: flex;
 		flex-flow: column;
