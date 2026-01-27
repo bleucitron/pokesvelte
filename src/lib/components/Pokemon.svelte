06de2802f5750c4b8fbded956fccf540fd9d6a6e
DylanNoally <dnoally@litesoft.fr>
2026-01-27 14:14:45 +0100

    Syntaxe

---
+++ src/lib/components/Pokemon.svelte
@@ -0,0 +1,9 @@
+<p>Pikachu</p>
+<p>#25</p>
+<img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png" alt="Pikachu">
+
+
+<style>
+    p { color: red;}
+    
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,6 @@
+<script lang="ts">
+    import Pokemon from "$lib/components/Pokemon.svelte";
+</script>
 <h1>Pokésvelte</h1>
 <p>Gotta svelt'em all!</p>
+<Pokemon />
