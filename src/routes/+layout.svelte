f037806de049669163b8b105a0d2f08105e317ae
Romain Crestey <rcrestey@gmail.com>
2026-01-27 12:17:34 +0100

    fix resolve

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,16 +1,19 @@
 <script lang="ts">
-	const { children, data } = $props();
 	import { page } from '$app/state';
 	import { resolve } from '$app/paths';
+	const { children, data } = $props();
 </script>
 
 <header>
 	<nav>
-
-		<a class={page.url.pathname === '/' ? 'current' : ''} href={resolve("/")}>home</a>
-		<a class={page.url.pathname === '/faq' ? 'current' : ''} href={resolve("/faq")}>faq</a>
-		<a class={page.url.pathname.startsWith('/pokedex') ? 'current' : ''} href={resolve("/pokedex")}>pokedex({data.nombrePokemonDecouvert}/{data.nombrePokemonTotal})</a>
-		<a class={page.url.pathname === '/team' ? 'current' : ''} href={resolve("/team")}>team({data.nombrePokemonEquipe})</a>
+		<a class={page.url.pathname === '/' ? 'current' : ''} href={resolve('/')}>home</a>
+		<a class={page.url.pathname === '/faq' ? 'current' : ''} href={resolve('/faq')}>faq</a>
+		<a class={page.url.pathname.startsWith('/pokedex') ? 'current' : ''} href={resolve('/pokedex')}
+			>pokedex({data.nombrePokemonDecouvert}/{data.nombrePokemonTotal})</a
+		>
+		<a class={page.url.pathname === '/team' ? 'current' : ''} href={resolve('/team')}
+			>team({data.nombrePokemonEquipe})</a
+		>
 	</nav>
 </header>
 
@@ -19,3 +22,4 @@
 </main>
 
 <footer>Mon footer</footer>
+
--- src/routes/pokedex/+page.svelte
+++ src/routes/pokedex/+page.svelte
@@ -5,12 +5,19 @@
 
 <h1>POKEDEX</h1>
 
-<a href={resolve('/pokedex/[id]', {
-	id: data.pokemons[0].id
-})}><img src={data.pokemons[0]?.sprites.front_default} alt="pokémon img"></a>
-<a href={resolve('/pokedex/[id]', {
-	id: data.pokemons[1].id
-})}><img src={data.pokemons[1]?.sprites.front_default} alt="pokémon img"></a>
-<a href={resolve('/pokedex/[id]', {
-	id:data.pokemons[2].id
-})}><img src={data.pokemons[2]?.sprites.front_default} alt="pokémon img"></a>
+<a
+	href={resolve('/pokedex/[id]', {
+		id: data.pokemons[0]?.id?.toString() ?? ''
+	})}><img src={data.pokemons[0]?.sprites.front_default} alt="pokémon img" /></a
+>
+<a
+	href={resolve('/pokedex/[id]', {
+		id: data.pokemons[1]?.id?.toString() ?? ''
+	})}><img src={data.pokemons[1]?.sprites.front_default} alt="pokémon img" /></a
+>
+<a
+	href={resolve('/pokedex/[id]', {
+		id: data.pokemons[2]?.id?.toString() ?? ''
+	})}><img src={data.pokemons[2]?.sprites.front_default} alt="pokémon img" /></a
+>
+
