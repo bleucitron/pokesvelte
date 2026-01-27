36d4a4c80717fdb16b57fa615f5f7116f0fb44ff
Stéphane DACHEUX <sdacheux@litesoft.fr>
2026-01-27 11:22:55 +0100

    Chargement des données de page

--- src/routes/pokedex/+page.svelte
+++ src/routes/pokedex/+page.svelte
@@ -1,1 +1,9 @@
+<script lang="ts">
+	const { data } = $props();
+</script>
+
 <h1>POKEDEX</h1>
+
+<img src={data.pokemons[0]?.sprites.front_default} alt="pokémon img">
+<img src={data.pokemons[1]?.sprites.front_default} alt="pokémon img">
+<img src={data.pokemons[2]?.sprites.front_default} alt="pokémon img">
