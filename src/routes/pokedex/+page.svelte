d5eb482249204d0aefd8ce05367744570cad6239
Romain Crestey <rcrestey@gmail.com>
2026-01-27 14:44:16 +0100

    add key

--- src/routes/pokedex/+page.svelte
+++ src/routes/pokedex/+page.svelte
@@ -5,28 +5,26 @@
 
 <h1>POKEDEX</h1>
 <ul>
-{#each data.pokemons as pokemon}
-
-	{@const src = pokemon.sprites.front_default}
-<li><a
-	href={resolve('/pokedex/[id]', {
-		id: pokemon.id.toString()
-	})}><img {src} alt="pokémon img" /></a
->
-</li>
-
+	{#each data.pokemons as pokemon (pokemon.id)}
+		{@const src = pokemon.sprites.front_default}
+		<li>
+			<a
+				href={resolve('/pokedex/[id]', {
+					id: pokemon.id.toString()
+				})}><img {src} alt="pokémon img" /></a
+			>
+		</li>
 	{/each}
 </ul>
 
 <style>
-    ul {
-        display: flex;
-        flex-wrap: wrap;
-        margin-block: 1rem;
-        gap: 1rem;
-    }
-    li:hover img {
-        filter: drop-shadow(0px 0px 10px #333);
-    }
+	ul {
+		display: flex;
+		flex-wrap: wrap;
+		margin-block: 1rem;
+		gap: 1rem;
+	}
+	li:hover img {
+		filter: drop-shadow(0px 0px 10px #333);
+	}
 </style>
-
