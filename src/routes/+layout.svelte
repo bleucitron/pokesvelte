bdda5f502c730d7ac29945d8a565dcee518826eb
RLHommee <rlhommee@litesoft.fr>
2026-01-27 12:09:57 +0100

    Ajout des liens dynamiques avec resolve

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,15 +1,16 @@
 <script lang="ts">
 	const { children, data } = $props();
 	import { page } from '$app/state';
+	import { resolve } from '$app/paths';
 </script>
 
 <header>
 	<nav>
 
-		<a class={page.url.pathname === '/' ? 'current' : ''} href="/">home</a>
-		<a class={page.url.pathname === '/faq' ? 'current' : ''} href="/faq">faq</a>
-		<a class={page.url.pathname.startsWith('/pokedex') ? 'current' : ''} href="/pokedex">pokedex({data.nombrePokemonDecouvert}/{data.nombrePokemonTotal})</a>
-		<a class={page.url.pathname === '/team' ? 'current' : ''} href="/team">team({data.nombrePokemonEquipe})</a>
+		<a class={page.url.pathname === '/' ? 'current' : ''} href={resolve("/")}>home</a>
+		<a class={page.url.pathname === '/faq' ? 'current' : ''} href={resolve("/faq")}>faq</a>
+		<a class={page.url.pathname.startsWith('/pokedex') ? 'current' : ''} href={resolve("/pokedex")}>pokedex({data.nombrePokemonDecouvert}/{data.nombrePokemonTotal})</a>
+		<a class={page.url.pathname === '/team' ? 'current' : ''} href={resolve("/team")}>team({data.nombrePokemonEquipe})</a>
 	</nav>
 </header>
 
--- src/routes/pokedex/+page.svelte
+++ src/routes/pokedex/+page.svelte
@@ -1,9 +1,16 @@
 <script lang="ts">
 	const { data } = $props();
+	import { resolve } from '$app/paths';
 </script>
 
 <h1>POKEDEX</h1>
 
-<img src={data.pokemons[0]?.sprites.front_default} alt="pokémon img">
-<img src={data.pokemons[1]?.sprites.front_default} alt="pokémon img">
-<img src={data.pokemons[2]?.sprites.front_default} alt="pokémon img">
+<a href={resolve('/pokedex/[id]', {
+	id: data.pokemons[0].id
+})}><img src={data.pokemons[0]?.sprites.front_default} alt="pokémon img"></a>
+<a href={resolve('/pokedex/[id]', {
+	id: data.pokemons[1].id
+})}><img src={data.pokemons[1]?.sprites.front_default} alt="pokémon img"></a>
+<a href={resolve('/pokedex/[id]', {
+	id:data.pokemons[2].id
+})}><img src={data.pokemons[2]?.sprites.front_default} alt="pokémon img"></a>
