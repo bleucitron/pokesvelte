053093aefdcc9c6d685b0a6bd31e795690644991
gDuval60 <gduval@litesoft.fr>
2026-01-27 10:52:44 +0100

    ajout du header

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	const { children } = $props();
+</script>
+
+<header>
+	<nav>
+		<a href="/">home</a>
+		<a href="/faq">faq</a>
+		<a href="/pokedex">pokedex</a>
+		<a href="/team">team</a>
+	</nav>
+</header>
+
+<main>
+	{@render children()}
+</main>
+
+<footer>Mon footer</footer>
