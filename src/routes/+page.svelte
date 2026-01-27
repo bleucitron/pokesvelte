05a01cf6096716f7320301ca81cc35546e6c9859
gDuval60 <gduval@litesoft.fr>
2026-01-27 16:36:28 +0100

    event commencer

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	const started = false;
+	let started = $state(false);
 	const { data } = $props();
 	import Wild from '$lib/components/Wild.svelte';
 </script>
@@ -7,7 +7,11 @@
 <h1>Pokésvelte</h1>
 <p>Gotta svelt'em all!</p>
 
-{#if started === false}
+<button onclick={()=> started= !started}>
+	{!started? 'commencer': 'arreter'}
+</button>
+
+{#if started}
 	{#each [1, 4, 7] as idpokemon (idpokemon)}
 		{@const pokemon = data.pokemons[idpokemon - 1]}
 		{#if pokemon}
