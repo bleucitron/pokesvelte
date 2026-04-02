a114fd534001e8d0436be87e9b04d1aa31e37e2a
RLHommee <rlhommee@litesoft.fr>
2026-04-02 16:44:42 +0200

    Soumission des datas du trainer

--- src/routes/trainer/+page.server.ts
+++ src/routes/trainer/+page.server.ts
@@ -1,3 +1,16 @@
+import db from '$lib/server/db';
+
 export function load() {
-    throw new Error("MissingNo est passé par la");
+	console.log('MissingNo est passé par la');
 }
+
+export const actions = {
+	default: async ({ request }) => {
+		const data = await request.formData();
+		const nom = data.get('nom');
+		const mdp = data.get('password');
+
+		let inscription = await db.trainer.register(nom,mdp)
+
+	}
+};
--- src/routes/trainer/+page.svelte
+++ src/routes/trainer/+page.svelte
@@ -1,1 +1,16 @@
+<script>
+</script>
+
 <h1>Dresseur</h1>
+
+<form method="POST">
+	<label for="nom">
+		Nom
+	</label>
+	<input id="nom" name="nom" placeholder="..." />
+	<label for="password">
+		Mot de passe
+	</label>
+	<input id="password" name="password" placeholder="******" type="password"/>
+	<button type="submit">Valider</button>
+</form>
