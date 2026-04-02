46c481169c9bceb9fe83ca832a4fb11c77e5f076
gDuval60 <gduval@litesoft.fr>
2026-04-02 16:21:28 +0200

    random position

--- src/lib/components/Wild.svelte
+++ src/lib/components/Wild.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
+	import { getRandomNb } from '$lib/utils';
+
 
 	type Props = {
 		name: string;
@@ -9,8 +11,23 @@
 	};
 	const { name, src, catchPokemon, escape }: Props = $props();
 
+
+	let top = $state(0)
+	let left = $state(0)
+
+	let innerHeight = $state(0)
+	let innerWidth = $state(0)
+
+
+$inspect(innerHeight,innerWidth)
+
 	onMount(() => {
 		if(escape) {
+
+
+			top = getRandomNb(0,innerHeight)
+			left = getRandomNb(0,innerWidth)
+
 			const time = setTimeout(() => {
 				escape();
 			}, 2000)
@@ -18,17 +35,19 @@
 		}
 	})
 
-	let innerHeight = $state(0)
-	let innerWidth = $state(0)
 
-	$inspect(innerHeight)
-	$inspect(innerWidth)
 
 </script>
 
-<button class={['Wild']} onclick={catchPokemon}>
+<button class={['Wild']} onclick={catchPokemon} style:top={top+"px"} style:left={left+"px"}>
 	<img {src} alt={name} />
 </button>
 
 <svelte:window bind:innerWidth bind:innerHeight />
 
+<style>
+	button {
+			position: fixed;
+	}
+
+</style>
