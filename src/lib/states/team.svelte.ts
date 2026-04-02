8acf36209080614c5ecd574c828f16897d2f7650
Romain Crestey <rcrestey@gmail.com>
2026-04-02 11:37:33 +0200

    team list

--- src/lib/states/team.svelte.ts
+++ src/lib/states/team.svelte.ts
@@ -1,10 +1,13 @@
-type Member = {id:number, uuid: number, name:string};
+type Member = { id: number; uuid: number; name: string };
 class Team {
 	members = $state<Member[]>([]);
 	addMember = (id: number) => {
 		this.members.push({ id, uuid: Date.now(), name: Date.now().toString() });
 	};
+	removeMember = (uuid: number) => {
+		this.members = this.members.filter((m) => m.uuid !== uuid);
+	};
 }
 
-
 export const team = new Team();
+
--- src/routes/team/+page.svelte
+++ src/routes/team/+page.svelte
@@ -1,1 +1,34 @@
+<script lang="ts">
+	const { data } = $props();
+	const { pokemons } = $derived(data);
+	import { team } from '$lib/states/team.svelte.js';
+</script>
+
 <h1>TEAM</h1>
+<ul>
+	{#each team.members as member (member.uuid)}
+		{@const p = pokemons[member.id - 1]}
+		{#if p}
+			{@const src = p.sprites.front_default}
+			<li>
+				<img {src} alt="pokémon img" />
+				<p>{member.name}</p>
+				<button
+					onclick={() => {
+						team.removeMember(member.uuid);
+					}}>Free</button
+				>
+			</li>
+		{/if}
+	{/each}
+</ul>
+
+<style>
+	ul {
+		display: flex;
+		flex-wrap: wrap;
+		margin-block: 1rem;
+		gap: 1rem;
+	}
+</style>
+
