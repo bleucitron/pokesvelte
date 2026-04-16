72ff5cd1742e09b290a8cc213841a7376729716d
DylanNoally <dnoally@litesoft.fr>
2026-04-16 09:48:34 +0200

    Exercice Validation Formulaire

--- src/routes/trainer/+page.server.ts
+++ src/routes/trainer/+page.server.ts
@@ -1,4 +1,5 @@
 import db from '$lib/server/db';
+import { fail } from '@sveltejs/kit';
 
 export function load() {
 	console.log('MissingNo est passé par la');
@@ -7,10 +8,34 @@ export function load() {
 export const actions = {
 	default: async ({ request }) => {
 		const data = await request.formData();
-		const nom = data.get('nom');
-		const mdp = data.get('password');
+		const nom = data.get('nom')?.toString();
+		const mdp = data.get('password')?.toString();
+		const mdpConf = data.get('passwordConf')?.toString();
 
-		let inscription = await db.trainer.register(nom,mdp)
+		if(!nom) {
+			return fail(400, {nom, champ: "nom", message: "Le nom est manquant." });
+		}
+		if(!mdp) {
+			return fail(400, {nom, champ: 'password', message: 'Le mot de passe est manquant.' });
+		}
+		if(mdp.length < 3) {
+			return fail(400, {nom, champ: 'password', message: 'Le mot de passe est trop court.' });
+		}
+		if(mdp !== mdpConf) {
+			return fail(400, {
+				nom,
+				champ: 'passwordConf',
+				message: 'Les mots de passe sont différents.'
+			});
+		}
 
+		const utilisateurExistant = await db.trainer.get(nom);
+		if (utilisateurExistant) {
+			return fail(400, {nom, champ: 'nom', message: "L'utilisateur existe déjà." });
+		}
+
+		await db.trainer.register(nom,mdp);
+
+		return { success: true };
 	}
 };
--- src/routes/trainer/+page.svelte
+++ src/routes/trainer/+page.svelte
@@ -1,16 +1,32 @@
 <script>
+	const { form } = $props();
 </script>
 
 <h1>Dresseur</h1>
 
+{#if form?.success}
+	<p>Formulaire envoyé avec succès !</p>
+{:else}
+	<p>{form?.message}</p>
+{/if}
 <form method="POST">
 	<label for="nom">
 		Nom
 	</label>
-	<input id="nom" name="nom" placeholder="..." />
+	<input id="nom" name="nom" placeholder="..."  class={{error: form?.champ === "nom"}} value={form?.nom}/>
 	<label for="password">
 		Mot de passe
 	</label>
-	<input id="password" name="password" placeholder="******" type="password"/>
+	<input id="password" name="password" placeholder="******" type="password" class={{error: form?.champ === "password"}}/>
+	<label for="passwordConf">
+		Confirmation du mot de passe
+	</label>
+	<input id="passwordConf" name="passwordConf" placeholder="******" type="password" class={{error: form?.champ === "passwordConf"}}/>
 	<button type="submit">Valider</button>
 </form>
+
+<style>
+	input.error {
+			border-color:red;
+	}
+</style>
