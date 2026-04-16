138727f9f23bf22789f6b5e437fdede0ee11eb80
gDuval60 <gduval@litesoft.fr>
2026-04-16 15:37:02 +0200

    exercice 2: array wild

--- src/lib/components/Grass.svelte
+++ src/lib/components/Grass.svelte
@@ -1,33 +1,44 @@
 <script lang="ts">
 
 	import Wild from '$lib/components/Wild.svelte';
-	import { getRandomNb } from '$lib/utils.ts';
+	import { getRandomNb } from '$lib/utils';
 
 	const { pokemons, catchPokemon } = $props();
 
-	let wild = $state<number>();
+	let wilds = $state< {id: number, appeared: number}[]>([]);
+
 	$effect(() => {
 		const interval =
 			setInterval(() => {
-				wild = getRandomNb(1, 151);
+				wilds.push({id: getRandomNb(1, 151), appeared: Date.now()});
 				console.log('rencontre');
 			}, 3000) ;
 
 		return () => clearInterval(interval);
 	});
 
+	function escape(date:number) {
+
+		wilds = wilds.filter((other) => {return other.appeared!==date})
+	}
+
 </script>
 
+{#each wilds as wild }
 
-{#if wild}
-	{@const pokemon = pokemons[wild - 1]}
-	{#if pokemon}
-		{@const { id, name, sprites } = pokemon}
-		<Wild
-			{name}
-			src={sprites.front_default}
-			catchPokemon={() => catchPokemon(id)}
-			escape={() => { wild = undefined }}
-		/>
-	{/if}
-{/if}
+		{@const pokemon = pokemons[wild.id - 1]}
+		{#if pokemon}
+			{@const { id, name, sprites } = pokemon}
+			<Wild
+				{name}
+				src={sprites.front_default}
+				catchPokemon={() => {
+					catchPokemon(id)
+					escape(wild.appeared)
+					}
+				}
+				escape={() => { escape(wild.appeared)}}
+			/>
+		{/if}
+
+{/each}
