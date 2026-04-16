84d32a3627662c53c789156f9bb4a4d4f5e365a9
DylanNoally <dnoally@litesoft.fr>
2026-04-16 16:05:30 +0200

    Snippets

--- src/routes/team/+page.svelte
+++ src/routes/team/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import { invalidate } from '$app/navigation';
 	import { enhance } from '$app/forms';
+	import { type TeamMember } from '$lib/server/db/team';
 
 	const { data } = $props();
 	const { pokemons, team } = $derived(data);
@@ -9,68 +10,43 @@
 </script>
 
 <h1>TEAM</h1>
-<ul>
-	{#each titulaire as member (member.uuid)}
-		{@const p = pokemons[member.id - 1]}
-		{#if p}
-			{@const src = p.sprites.front_default}
-			<li>
-				<img {src} alt="pokémon img" />
-				<p>{member.name}</p>
-
-				<form action="?/rename" method="POST" use:enhance>
-					<label for="nomPkm">
-						Nom
-					</label>
-					<input id="nomPkm" name="nomPkm" placeholder="..."/>
-					<input name="uuid" type="hidden" value="{member.uuid}"/>
-					<button type="submit">Valider</button>
-					<button formaction="?/titulaire">{member.main ? 'Mettre au dodo' : 'Mettre au boulot' }</button>
-				</form>
-				<button
-					onclick={async () => {
-						// team.removeMember(member.uuid);
-								await fetch("/team/"+ member.uuid, {method:"DELETE"})
-								invalidate("team:update");
-					}}>Free</button
-				>
-			</li>
-		{/if}
-	{/each}
-</ul>
+{@render memberList(titulaire)}
 
 <h2>BANC</h2>
-<ul>
-	{#each remplacant as member (member.uuid)}
-		{@const p = pokemons[member.id - 1]}
-		{#if p}
-			{@const src = p.sprites.front_default}
-			<li>
-				<img {src} alt="pokémon img" />
-				<p>{member.name}</p>
+{@render memberList(remplacant)}
 
-				<form action="?/rename" method="POST" use:enhance>
-					<label for="nomPkm">
-						Nom
-					</label>
-					<input id="nomPkm" name="nomPkm" placeholder="..."/>
-					<input name="uuid" type="hidden" value="{member.uuid}"/>
-					<button type="submit">Valider</button>
-					<button formaction="?/titulaire">{member.main ? 'Mettre au dodo' : 'Mettre au boulot' }</button>
-				</form>
 
-				<button
-					onclick={async () => {
+{#snippet memberList(members:TeamMember[])}
+	<ul>
+		{#each members as member (member.uuid)}
+			{@const p = pokemons[member.id - 1]}
+			{#if p}
+				{@const src = p.sprites.front_default}
+				<li>
+					<img {src} alt="pokémon img" />
+					<p>{member.name}</p>
+
+					<form action="?/rename" method="POST" use:enhance>
+						<label for="nomPkm">
+							Nom
+						</label>
+						<input id="nomPkm" name="nomPkm" placeholder="..."/>
+						<input name="uuid" type="hidden" value="{member.uuid}"/>
+						<button type="submit">Valider</button>
+						<button formaction="?/titulaire">{member.main ? 'Mettre au dodo' : 'Mettre au boulot' }</button>
+					</form>
+					<button
+						onclick={async () => {
 						// team.removeMember(member.uuid);
 								await fetch("/team/"+ member.uuid, {method:"DELETE"})
 								invalidate("team:update");
 					}}>Free</button
-				>
-			</li>
-		{/if}
-	{/each}
-</ul>
-
+					>
+				</li>
+			{/if}
+		{/each}
+	</ul>
+{/snippet}
 <style>
 	ul {
 		display: flex;
