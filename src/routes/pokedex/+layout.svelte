04898f589e953b007c49d96ff86e055c56d7a8fb
Romain Crestey <rcrestey@gmail.com>
2026-04-16 13:53:57 +0200

    create common layout for Pokedex

---
+++ src/routes/pokedex/+layout.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	import Pokedex from '$lib/components/Pokedex.svelte';
+	const { data } = $props();
+	const { pokemons, pokedex } = $derived(data);
+</script>
+
+<Pokedex {pokemons} {pokedex} />
--- src/routes/pokedex/+page.svelte
+++
@@ -1,7 +0,0 @@
-<script lang="ts">
-	import Pokedex from '$lib/components/Pokedex.svelte';
-	const { data } = $props();
-	const { pokemons , pokedex} = $derived(data);
-</script>
-
-<Pokedex {pokemons} {pokedex} />
