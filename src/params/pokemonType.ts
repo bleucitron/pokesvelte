8dedc5fd6aeb0d8c5d8a0c14977e091ebf4d519a
Romain Crestey <rcrestey@gmail.com>
2026-04-16 13:49:22 +0200

    optim pokedex load

--- src/params/pokemonType.ts
+++ src/params/pokemonType.ts
@@ -1,25 +1,24 @@
 import { fetchPokemonTypes } from '$lib/pokemons';
 type PokemonType =
-	'grass' |
-	'poison' |
-	'fire' |
-	'flying' |
-	'water' |
-	'bug' |
-	'normal' |
-	'electric' |
-	'ground' |
-	'fairy' |
-	'fighting' |
-	'psychic' |
-	'rock' |
-	'steel' |
-	'ice' |
-	'ghost' |
-	'dragon ';
-	const types = await fetchPokemonTypes();
+	| 'grass'
+	| 'poison'
+	| 'fire'
+	| 'flying'
+	| 'water'
+	| 'bug'
+	| 'normal'
+	| 'electric'
+	| 'ground'
+	| 'fairy'
+	| 'fighting'
+	| 'psychic'
+	| 'rock'
+	| 'steel'
+	| 'ice'
+	| 'ghost'
+	| 'dragon ';
+const types = await fetchPokemonTypes();
 
-export function match(param: string): param is PokemonType  {
-
+export function match(param: string): param is PokemonType {
 	return types.includes(param);
 }
--- src/routes/pokedex/+layout.server.ts
+++ src/routes/pokedex/+layout.server.ts
@@ -1,21 +1,18 @@
 import { fetchPokemons, fetchPokemonTypes } from '$lib/pokemons';
 import db from '$lib/server/db';
 
-export async function load({params}) {
+export async function load({ params }) {
 	const type = params.type;
-	// const id = params.id;
-	const types = await fetchPokemonTypes();
 
-	const [pokemons, pokedex] = await Promise.all([fetchPokemons(), db.seen.get()]);
+	const [pokemons, pokedex, types] = await Promise.all([
+		fetchPokemons(),
+		db.seen.get(),
+		fetchPokemonTypes()
+	]);
 
-		const pokemonsToSend = type ?
-			pokemons.filter(
-				pokemon =>
-				pokemon.types.find(
-					t =>
-						t.type.name === type
-				)
-			) : pokemons;
+	const pokemonsToSend = type
+		? pokemons.filter((pokemon) => pokemon.types.find((t) => t.type.name === type))
+		: pokemons;
 
-	return { pokemons : pokemonsToSend, pokedex, types };
+	return { pokemons: pokemonsToSend, pokedex, types };
 }
