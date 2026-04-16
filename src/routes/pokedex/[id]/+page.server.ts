0e35348c25f7341fccd0e417a8b16644e23337f7
JVLS34 <jvigier@litesoft.fr>
2026-04-16 11:30:05 +0200

    Redirection erreur

--- src/routes/pokedex/[id]/+page.server.ts
+++ src/routes/pokedex/[id]/+page.server.ts
@@ -1,9 +1,16 @@
 import { fetchPokemon } from '$lib/pokemons';
 import db from '$lib/server/db';
+import { error } from '@sveltejs/kit';
+
 export async function load({params}) {
 	const id = params.id;
 	const [pokedex, pokemon] = await  Promise.all([db.seen.get(), fetchPokemon(id)]);
 	const found = pokedex.includes(parseInt(id));
+
+	if (!pokemon)
+	{
+		error(404, { message: "Aucun pokemon trouvé"});
+	}
 
 	return { pokemon, found };
 }
